b3ead486f9a344b923b41ca46268b89fd95e46f6
hinell <al.neodim@gmail.com>
2024-08-20 15:36:38 +0300

    feat(chrome): extension v3 manifest support

--- README.md
+++ README.md
@@ -9,7 +9,18 @@ to your extensions bar which will open up your Bookmark Manager in a new tab on 
 
 ### Credits 
 
-Thanks to unknown authors of this extension. It had disappeared from google web store so I have saved it. Currently only `manifest v2` is published. I may update it in the future. I didn't make a `.crx` archive due to lack of time on hands. 
+Thanks to unknown authors of this extension. It had disappeared from google web store so I have saved it. Currently both Google Extension Manifest `v2` and `v3` are supported.  
+If you need `v2` for older browsers, download a `.zip` file of the `v1.3` version. 
+
+
+### Download & Install
+
+1. Head over to **Tags** page
+2. Find an appropriate release tag (.e.g `v1.4`)
+3. Download `.zip` and unpack the archive
+4. Head over to chrome window, open extensions manager
+5. Toggle developer mode, then, **load unpacked** 
+6. Specify path to the unpacked archive from step 3
 
 -----
 
--- background.js
+++ background.js
@@ -1,28 +1,26 @@
-
 // Opens the bookmarks page
-
+// API proxy required by google chrome extension v3 
+let localStorage = chrome.storage.local
 function OpenBookmarks() {
- 
-	if (localStorage["url"] !== undefined){
-		chrome.tabs.create({url:"chrome://bookmarks/#" + localStorage["url"]});		
-	} else {
-		chrome.tabs.create({url:"chrome://bookmarks"});
-	}
+
+	localStorage.get("url").then(function (v){
+		if (v.url !== undefined){
+			chrome.tabs.create({ url:"chrome://bookmarks/#" + v.url });		
+		} else {
+			chrome.tabs.create({url:"chrome://bookmarks"});
+		}
+	})
 }
-
-
-
 
 // Adds the listener for the toolbar button
-
-chrome.browserAction.onClicked.addListener(OpenBookmarks);
-
-
-
+chrome.action.onClicked.addListener(OpenBookmarks);
 
 // Run this on first run to setup default bookmarks and explain how to set up keyboard shortcuts
-
-if (localStorage["firstRun"] === undefined){
-	chrome.tabs.create({url:"options.html"});
-	localStorage["firstRun"] = false;
-}
+localStorage.get("firstRun").then(function (v){
+	if (v.firstRun == null){
+		chrome.tabs.create({url:"options.html"});
+		localStorage.set({ firstRun: true })
+	} else {
+		console.log("Open Bookmarks Manager is run second time ;)")
+	}
+})
